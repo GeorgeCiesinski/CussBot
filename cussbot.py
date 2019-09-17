b01b613e4b7f5dc0f867430813b6e9bd97719c48
George Ciesinski <george.ciesin@gmail.com>
2019-09-17 10:35:37 -0400

    Added Todo reminders

--- cussbot.py
+++ cussbot.py
@@ -5,13 +5,15 @@ import configparser
 class CussFinder:
 
     def __init__(self):
+        # Set in praw_login()
+        self.reddit = None
         self.praw_login()
         self.comment_skimmer()
 
     def praw_login(self):
 
         # Read praw.ini config file for login info
-        # Todo: Check if config file exists. Create if does not.
+        # Todo: Check if config file exists. Create if does not
         config = configparser.RawConfigParser()
         config.read('Config/praw.ini')
 
@@ -33,12 +35,15 @@ class CussFinder:
 
         print("PRAW Login Successful.")
 
+    # Todo: Create new .py file for the comment skimmer
     def comment_skimmer(self):
 
         # Subreddits | Subreddits go here
         subreddit = self.reddit.subreddit('funny')
 
         # Keyphrase | Swear words go here in alphabetical order
+        # Todo: Add more swear words
+        # Todo: Add conditional options depending on swear word origin
         keyphrase = ['asshole',
                      'bastard',
                      'bitch',
@@ -49,6 +54,7 @@ class CussFinder:
                      'slut',
                      'twat']
 
+        # Todo: Update log instead of printing to console
         # Look through comments in subreddit and print info to shell
         for comment in subreddit.stream.comments():
             for cuss in keyphrase:
