22834286c2a03beebf72ca0b22a8e0821f562634
George Ciesinski <george.ciesin@gmail.com>
2019-10-08 21:08:20 -0400

    Added new methods to create word tables.

--- Database.py
+++ Database.py
@@ -27,6 +27,7 @@ class Database:
         # Attempt to create connection object, log exception if fails.
         try:
             conn = sqlite3.connect(db_directory)
+            logger.info('Successfully created connection object.')
             return conn
         except:
             logger.exception('Failed to create connection object.')
@@ -35,14 +36,46 @@ class Database:
         # Returns connection object
         return conn
 
-    # Creates word table for the first time
     @staticmethod
-    def create_word_tables(conn):
-        # Connection object
-        c = conn
+    def close_conn(conn):
+        try:
+            conn.close()
+            logger.info('Successfully closed database connection.')
+        except:
+            logger.exception('Database connection failed to close.')
+            raise
+
+    @staticmethod
+    def execute_query(conn, query):
+        # Executes sql query
+        try:
+            c = conn.cursor()
+            c.execute(query)
+            conn.commit()
+            logger.info('Successfully executed the query.')
+        except:
+            logger.exception(f'Failed to execute the query: \n{query}')
+            raise
+
+    # Creates word table for the first time
+    def create_word_tables(self, conn):
+
+        sql_create_cusswords_table = """CREATE TABLE IF NOT EXISTS cusswords (
+        id integer PRIMARY KEY,
+        word text NOT NULL
+        );"""
+
+        sql_create_property_table = """CREATE TABLE IF NOT EXISTS property (
+        word_id integer NOT NULL,
+        property_name varchar(100) NOT NULL,
+        property_value varchar(100) NOT NULL
+        );"""
+
+        self.execute_query(conn, sql_create_cusswords_table)
+        self.execute_query(conn, sql_create_property_table)
 
     def start_database(self):
-        
+
         """
         This method always needs to be run first.
         - Creates db if doesn't exist
@@ -55,13 +88,21 @@ class Database:
         # Checks if database exists
         if os.path.exists(db_directory):
             logger.info('Database has been found at ' + db_directory + '.')
-            # Creates connection object.
-            conn = self.create_connection(db_directory)
         else:
             logger.info('Database not found. Creating new database.')
             # Creates connection object. This also creates the database if doesn't exist.
             conn = self.create_connection(db_directory)
-            logger.info('Database successfully created.')
+            logger.info('Successfully created database.')
+            self.close_conn(conn)
+
+        # Create connection
+        conn = self.create_connection(db_directory)
+
+        # Create word tables
+        self.create_word_tables(conn)
+
+        # Close connection
+        self.close_conn(conn)
 
 
 # Logger setup
